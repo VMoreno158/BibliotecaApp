d946c4ca72d5c612aac9093cc4667bf05347d990
VMoreno158 <vmorenode.inf@upsa.es>
2025-03-09 15:44:58 +0100

    Innitial commit

--- bibliotecaApp/asgi.py
+++ bibliotecaApp/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for cochesApp project.
+ASGI config for bibliotecaApp project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cochesApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bibliotecaApp.settings')
 
 application = get_asgi_application()
--- bibliotecaApp/wsgi.py
+++ bibliotecaApp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for cochesApp project.
+WSGI config for bibliotecaApp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cochesApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bibliotecaApp.settings')
 
 application = get_wsgi_application()
