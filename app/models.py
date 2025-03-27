08c86ec574355ebd58da409187b87befc3f347de
VMoreno158 <vmorenode.inf@upsa.es>
2025-03-27 12:17:44 +0100

    commit fixModelFields

--- app/models.py
+++ app/models.py
@@ -51,7 +51,7 @@ class Book(models.Model):
 
 class User(models.Model):
     dni = models.CharField(max_length=9, unique=True)
-    email = models.CharField(max_length=100)
+    email = models.EmailField()
     telf_number = models.CharField(max_length=9)
     name = models.CharField(max_length=50)
     surname = models.CharField(max_length=100)
